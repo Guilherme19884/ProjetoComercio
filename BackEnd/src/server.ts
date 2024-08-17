8c237872558c70e382400c219eab65cffc5e4835
Guilherme <guilherme1984.araujo@gmail.com>
2024-08-16 21:17:23 -0300

    criado o servidor

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,16 +1,25 @@
-import express from "express";
+import express, { Request, Response, urlencoded } from "express";
 import cors from "cors";
 import dotenv from "dotenv"
 import { connectToDatabase } from "./database/database";
+import { Router } from "express";
 
-
-const app = express()
-app.use(cors())
-app.use(express.json())
 dotenv.config()
 connectToDatabase()
 
-const port = process.env.PORT 
+const router = Router()
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(urlencoded({extended: true}))
+app.use('/', router)
+
+
+router.get('/', async ( req: Request, res: Response)=>{
+    return res.status(200).json({message: " 🚀 Bem vindo ao seu servidor! 🚀"}) 
+})
+
+const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
     console.log(`Servidor rodando na porta ${port}✅`)
