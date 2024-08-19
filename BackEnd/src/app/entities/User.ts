c9be30971ffa26d7827b6415b7d1702de7211717
Guilherme <guilherme1984.araujo@gmail.com>
2024-08-19 16:05:31 -0300

    corrigido pequenos erros de digitação

--- BackEnd/src/app/entities/User.ts
+++ BackEnd/src/app/entities/User.ts
@@ -2,19 +2,19 @@ import mongoose, { Schema } from "mongoose";
 
 // Definindo o schema para o usuário
 const UserSchema: Schema = new Schema({
- rol: { type: String, maxlength: 30, required: true},
+  role: { type: String, maxlength: 30, required: true }, // Corrigido de "rol" para "role"
   name: { type: String, maxlength: 250, required: true },
   email: { type: String, maxlength: 250, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   avatar: { type: String, maxlength: 250, required: true },
-  state: { type: Number, dafault:1 }, //1 está ativo e 2 está inativo
-  phone: { type: String, maxlength: 20, required: false},
-  birthday: { type: String, maxlength: 20, required: false}
+  state: { type: Number, default: 1 }, // Corrigido "dafault" para "default"
+  phone: { type: String, maxlength: 20, required: false },
+  birthday: { type: String, maxlength: 20, required: false }
 });
 
 // Criando o modelo com o schema definido
-const User = mongoose.model("User", UserSchema)
+const User = mongoose.model("User", UserSchema);
 
-export default User
+export default User;
